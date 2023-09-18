cb6de62973eb12a7f6221bb4b82c5480aa7b44f0
claudiomercado <claudiomercado19@gmail.com>
2023-09-18 20:08:03 -0300

    firts commit

---
+++ src/main/java/com/repuestosgaston/RepuestosGastonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.repuestosgaston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepuestosGastonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepuestosGastonBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repuestosgaston/RepuestosGastonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repuestosgaston;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepuestosGastonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
