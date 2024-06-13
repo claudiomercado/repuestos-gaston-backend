f4da2658872e99031cd380764748791e3dcfeb2d
mariaines20397 <mariainestoledo20397@gmail.com>
2024-06-13 01:33:17 -0300

    add enum order sale

--- src/main/java/com/repuestosgaston/shopping_cart/model/enums/SaleOrderStatusEnum.java
+++ src/main/java/com/repuestosgaston/shopping_cart/model/enums/SaleOrderStatusEnum.java
@@ -2,7 +2,7 @@ package com.repuestosgaston.shopping_cart.model.enums;
 
 public enum SaleOrderStatusEnum {
 
-	PENDING_PAYMENT("Pendiente de pago"), PAID("Pagado"), REJECTED("Rechazado");
+	PENDING_PAYMENT("Pendiente de pago"), PAID("Pagado"), REJECTED("Rechazado"), DELIVERED("Entregado");
 	
 	private final String name;
 	
